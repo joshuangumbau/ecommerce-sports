4600e925bd29ab51263bd461e05075f5809b899e
joshie ngumbau <ngumbaujoshua4@gmail.com>
2023-06-13 13:23:20 +0300

    new changes

--- src/LandingPage/Header.jsx
+++ src/LandingPage/Header.jsx
@@ -12,7 +12,7 @@ function Header() {
   return (
     <div className="fixed top-0 left-0 right-0 z-10 bg-white p-2 ">
         <div className="container w-10/12 h-[10vh] flex items-center  m-auto justify-between">
-              <img src={logo} alt="logo" className='w-[150px]' />
+              <img src={logo} alt="sirojoskysports" className='w-[300px]' />
            <Link to="/SignUp">
            <button className="md:hidden cta bg-[#405DF8] text-[#fff] rounded-md px-10 hover:bg-black py-3">Get Started</button>
          
--- src/LandingPage/LandingPage.jsx
+++ src/LandingPage/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './Header'
-import Heroimg from '../assets/HeroImage.svg';
+import Heroimg from '../assets/heroimage.jpg';
 import HRX from '../assets/hrx.svg';
 import Nike from '../assets/Nike.svg';
 import Puma from '../assets/Puma.svg';
@@ -14,9 +14,9 @@ function LandingPage() {
      <Header />
       <div className=" w-10/12 md:py-[10vh] md:grid-cols-1 m-auto grid grid-cols-3 gap-4  hero mt-[10vh]">
             <div className="col1 flex flex-col justify-center items-start h-[90vh] md:h-[auto] md:my-[5vh]">
-              <p className='text-[#405DF8]'><i className='bx bxs-rocket'></i> Fast Shipping all over india</p>
+              <p className='text-[#405DF8]'><i className='bx bxs-rocket'></i> Fast Shipping all over kenya</p>
               <h1 className='font-bold text-6xl text-left font-serif'>Win with the best sports gear</h1>
-              <p className='text-left text-xl opacity-50 my-5 text-'>We've tied up with the best sports brand all over india to give you high quality & premium sports gear</p>
+              <p className='text-left text-xl opacity-50 my-5 text-'>We've tied up with the best sports brand all over kenya to give you high quality & premium sports gear</p>
                <Link to='/Signup'>
                <button className ="cta bg-[#405DF8] w-full text-[#fff] rounded-md px-10 hover:bg-black py-3">Get Started</button>
 
@@ -29,7 +29,7 @@ function LandingPage() {
               <h1 className='text-6xl font-serif font-bold'>100k +</h1>
               <p className='opacity-50 text-xl '> Daily products sold on our website</p>
               <div className="flex text-white card bg-[#1A1919] rounded-md p-4 my-5">
-              <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/a1a5ba116194407.605ccd8b1071c.png" alt="" className='w-[90px] h-[90px] rounded-full ' />
+              <img src="https://img.freepik.com/free-vector/color-sport-sneaker_98292-3191.jpg?size=626&ext=jpg" alt="" className='w-[90px] h-[90px] rounded-full ' />
              <div className="txt px-2  mx-2">
                <h2 className='text-left font-bold font-serif my-1 text-2xl'>Air Jordan 14 </h2>
                <p className='text-left opacity-70 text-md'>The Air Jordan XXXVI isn't just the next shoe up franchise</p>
